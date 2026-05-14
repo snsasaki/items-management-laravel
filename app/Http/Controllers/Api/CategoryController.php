8057a57477b2c10175d1fb9ba0b9c7357b2509a2
snsasaki <sn.sasaki@core-tech.jp>
2026-05-14 13:24:36 +0900

    :sparkles: 特定のパスで特定のカテゴリを取得できる機能を追加

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -31,9 +31,9 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
 
     /**
