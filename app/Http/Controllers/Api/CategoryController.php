9fb290926e55a911156976eb5ebd9d5875914d92
snsasaki <sn.sasaki@core-tech.jp>
2026-05-14 13:16:41 +0900

    :sparkles: requestとresponseの型を定義、カテゴリー一覧取得機能を追加

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,7 +14,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::latest()
+            ->get();
+
+        return CategoryResource::collection($categories);
     }
 
     /**
