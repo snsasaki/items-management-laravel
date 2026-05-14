35a1b92dffe14e00b3264372c3e659f756b7c728
snsasaki <sn.sasaki@core-tech.jp>
2026-05-14 13:36:16 +0900

    :sparkles: カテゴリを新規登録できる機能を追加

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -23,9 +24,14 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $category = Category::create($request->validated());
+
+        return response()->json([
+            'message' => 'カテゴリを新規登録しました。',
+            'data' => new CategoryResource($category),
+        ], 201);
     }
 
     /**
