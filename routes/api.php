282d699ca16a19a4c832504799294a37811099a6
snsasaki <sn.sasaki@core-tech.jp>
2026-05-14 11:51:05 +0900

    :sparkles: CategoryControllerを用意してルーティング

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('items', ItemController::class);
+
+Route::apiResource('categories', CategoryController::class);
