9b10da70e985ef3200d727ef3483e7ee2faab8be
snsasaki <sn.sasaki@core-tech.jp>
2026-05-14 10:52:56 +0900

    :sparkles: 特定の備品取得機能作成

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -57,9 +57,9 @@ class ItemController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Item $item): ItemResource
     {
-        //
+        return new ItemResource($item);
     }
 
     /**
