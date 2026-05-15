00207bd4774b4699ba14cf32eeda960834d2bbc3
snsasaki <sn.sasaki@core-tech.jp>
2026-05-15 13:08:09 +0900

    :sparkles: meルートを追加

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('items', ItemController::class);
 });
 
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return response()->json([
+        'user' => [
+            'id' => $request->user()->id,
+            'name' => $request->user()->name,
+            'email' => $request->user()->email,
+        ],
+    ]);
+});
+
 Route::apiResource('categories', CategoryController::class);
